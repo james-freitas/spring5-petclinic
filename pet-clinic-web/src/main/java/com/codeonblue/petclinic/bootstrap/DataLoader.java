cf9307c6deb9c026a7fc436f492983e01922d27f
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2019-09-13 23:07:34 -0300

    Added PetTypes to boostrap. closes #32

--- pet-clinic-web/src/main/java/com/codeonblue/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/codeonblue/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.codeonblue.petclinic.bootstrap;
 
 import com.codeonblue.petclinic.model.Owner;
+import com.codeonblue.petclinic.model.PetType;
 import com.codeonblue.petclinic.model.Vet;
 import com.codeonblue.petclinic.service.OwnerService;
+import com.codeonblue.petclinic.service.PetTypeService;
 import com.codeonblue.petclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
