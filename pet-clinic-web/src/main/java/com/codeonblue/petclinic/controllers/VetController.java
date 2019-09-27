d4d4eba71d8bd5a7a0e6d6f6bc13bd1648d89b69
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2019-09-27 08:45:48 -0300

    Fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/com/codeonblue/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/codeonblue/petclinic/controllers/VetController.java
@@ -15,7 +15,7 @@ public class VetController {
     }
 
 
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
