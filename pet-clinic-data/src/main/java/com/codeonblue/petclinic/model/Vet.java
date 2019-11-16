72096f4cca7f256629199534f2cc4978eb734e07
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2019-11-16 13:49:38 -0300

    updated vet to use JPA. closes #37

--- pet-clinic-data/src/main/java/com/codeonblue/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/codeonblue/petclinic/model/Vet.java
@@ -1,10 +1,20 @@
 package com.codeonblue.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
