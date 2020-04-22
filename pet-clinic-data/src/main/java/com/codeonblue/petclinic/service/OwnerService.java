05ad42c1dd6a0fe318f2123bffd345488a336217
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-04-21 21:23:47 -0300

    Find owner feature

--- pet-clinic-data/src/main/java/com/codeonblue/petclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/codeonblue/petclinic/service/OwnerService.java
@@ -2,7 +2,11 @@ package com.codeonblue.petclinic.service;
 
 import com.codeonblue.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
