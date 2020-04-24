dda92ca24f3e4a9d60d0cbe1fd3a058ff9be631c
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-04-24 10:18:33 -0300

    adding model attributes for Pet Controller

--- pet-clinic-data/src/main/java/com/codeonblue/petclinic/service/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/codeonblue/petclinic/service/PetTypeService.java
@@ -2,5 +2,7 @@ package com.codeonblue.petclinic.service;
 
 import com.codeonblue.petclinic.model.PetType;
 
+import java.util.Collection;
+
 public interface PetTypeService extends CrudService<PetType, Long> {
 }
