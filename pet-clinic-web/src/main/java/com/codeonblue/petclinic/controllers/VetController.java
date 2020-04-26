f9f0270bece6e8e6e67249a06120038b4177ea9d
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-04-26 01:30:42 -0300

    Implement Vets JSON endpoint

--- pet-clinic-web/src/main/java/com/codeonblue/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/codeonblue/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.codeonblue.petclinic.controllers;
 
+import com.codeonblue.petclinic.model.Vet;
 import com.codeonblue.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
